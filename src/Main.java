79f1f081306b72bdcca95c4f801442924ac44876
rjwvandenberg <rjwvandenberg@gmail.com>
2013-10-17 23:40:07 +0200

    Commit stub.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Boggle Saga!");
         System.out.println("Robert is awesome test commit 1");
+        System.out.println("CommitStub");
     }
 }

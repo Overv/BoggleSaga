a98dbb8a4dea2792b4b691dae809e251e72995e4
Alexander Overvoorde <overv161@gmail.com>
2013-10-19 00:52:24 +0200

    Add basic layout

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+import View.GameFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Boggle Saga!");
-        System.out.println("Robert is awesome test commit 1");
-        System.out.println("CommitStub");
+        new GameFrame();
     }
 }

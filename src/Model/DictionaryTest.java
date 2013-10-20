262f21d5ad7d7eebeb6fce77000d2709c3485691
Alexander Overvoorde <overv161@gmail.com>
2013-10-20 21:17:26 +0200

    Add dictionary JUnit test

---
+++ src/Model/DictionaryTest.java
@@ -0,0 +1,21 @@
+package Model;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Overv
+ * Date: 20-10-13
+ * Time: 21:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class DictionaryTest {
+    @Test
+    public void testCheckWord() throws Exception {
+        Dictionary dictionary = Dictionary.getInstance();
+
+        assert dictionary.checkWord("car");
+        assert !dictionary.checkWord("foo");
+        assert !dictionary.checkWord("fuck");
+    }
+}

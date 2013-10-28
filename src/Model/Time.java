83d74d8810683f3491c9cda885d3c0c8b16b53bf
Alexander Overvoorde <overv161@gmail.com>
2013-10-28 03:03:35 +0100

    Revert game time to 60

--- src/Model/Time.java
+++ src/Model/Time.java
@@ -13,7 +13,7 @@ public class Time {
     
     private Timer timer;
     private Timer secondsTimer;
-    private int gameDuration = 1; // in seconds
+    private int gameDuration = 60; // in seconds
     private int timeLeft;
     private TimeListener timeListener;
     
